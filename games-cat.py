51f413aeb5fe28b387404b307e65a825e973d6d8
Plamen Vatev <pazoff@gmail.com>
2024-02-18 17:48:35 +0200

    Add files via upload

---
+++ games-cat.py
@@ -0,0 +1,27 @@
+from cat.mad_hatter.decorators import tool, hook, plugin
+from cat.log import log
+
+@hook
+def agent_fast_reply(fast_reply, cat):
+    return_direct = True
+    
+    # Get user message from the working memory
+    message = cat.working_memory["user_message_json"]["text"]
+
+    if message.startswith('@games'):
+        
+            return {"output": 'Type: @trivia or @cookie or @puzzle'}
+
+    if message.startswith('@trivia'):
+        
+            return {"output": ' <iframe height="650" width="800" src="https://idev.games/appvert/game/1872/game54902/" title="Trivia"></iframe> '}
+    
+    if message.startswith('@cookie'):
+        
+            return {"output": ' <iframe height="650" width="800" src="https://www.smart-cookie-trivia.com/" title="Smart Cookie"></iframe> '}
+    
+    if message.startswith('@puzzle'):
+        
+            return {"output": ' <iframe height="650" width="800" src="https://html-classic.itch.zone/html/6586329/index.html" title="Puzzle"></iframe> '}
+
+    return None
